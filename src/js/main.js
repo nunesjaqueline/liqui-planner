9598060617f5abe4085bbe067af0230a4331b76d
Jaqueline Nunes-Heissenberger <anunes.jaqueline@gmail.com>
2024-05-15 22:20:27 +0200

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+// Eingabedaten holen
+let title_1 = prompt("Titel:");
+console.log(title_1);
+let typ_1 = prompt("Type (Einahme oder Ausgabe):");
+console.log(typ_1);
+let amount_1 = prompt("Betrag (in Cent):");
+console.log(amount_1);
+let date_1 = prompt("Datum (jjjj-mm-tt):");
+console.log(date_1);
+
+//Gesamtbilanz
+let income;
+let expense;
+let balance;
