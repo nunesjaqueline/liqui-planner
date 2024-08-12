378fddee4837915df217d397f871a7891921be4b
Jaqueline Nunes-Heissenberger <anunes.jaqueline@gmail.com>
2024-08-12 22:04:34 +0200

    Add some variables

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,29 @@
 "use strict";
 // Eingabedaten holen
 let title_1 = prompt("Titel:");
-console.log(title_1);
-let typ_1 = prompt("Type (Einahme oder Ausgabe):");
-console.log(typ_1);
-let amount_1 = prompt("Betrag (in Cent):");
-console.log(amount_1);
+let typ_1 = prompt("Typ (Einahme oder Ausgabe):");
+let betrag_1 = parseInt(prompt("Betrag (in Cent):"));
 let date_1 = prompt("Datum (jjjj-mm-tt):");
-console.log(date_1);
+
+console.log(`Titel: ${title_1}
+    Typ: ${typ_1}
+    Betrag: ${betrag_1} ct
+    Datum: ${date_1}`);
+
+let titel_2 = prompt("Titel: ");	
+let typ_2 = prompt("Typ (Einahme oder Ausgabe):");
+let betrag_2 = parseInt(prompt("Betrag (in Cent): "));
+let date_2 = prompt("Datum (jjjj-mm-tt): ");
+console.log(`Titel: ${titel_2}
+    Typ: ${typ_2}
+    Betrag: ${betrag_2}
+    Date: ${date_2}`);	
 
 //Gesamtbilanz
 let income;
 let expense;
-let balance;
+let balance = betrag_1 + betrag_2;
+console.log(`Gesamtbilanz: ${balance}`);
+
+let positiv = balance >= 0;
+console.log(`Bilanz ist positiv: ${positiv}`);
