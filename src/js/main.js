f075f0bd31b516d97479fb1dc0d06c3bba2c2870
Jaqueline Nunes-Heissenberger <anunes.jaqueline@gmail.com>
2024-08-23 11:43:14 +0200

    Add small changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,30 @@
 "use strict";
+let income = 0;
+let expense = 0;
+let balance = 0;
 // Eingabedaten holen
 let title_1 = prompt("Titel:");
 let typ_1 = prompt("Typ (Einahme oder Ausgabe):");
 let betrag_1 = parseInt(prompt("Betrag (in Cent):"));
 let date_1 = prompt("Datum (jjjj-mm-tt):");
 
+
 console.log(`Titel: ${title_1}
     Typ: ${typ_1}
     Betrag: ${betrag_1} ct
-    Datum: ${date_1}`);
+    Datum: ${date_1}`
+);
+
+if (typ_1 === "Einnahme") {
+    income = income + betrag_1;    	
+} else if (typ_1 === "Ausgabe") {
+    expense = expense + betrag_1;
+    balance = balance - betrag_1;
+
+} else {
+    console.log(`Der Typ_"${typ_1}" ist nicht bekannt`);
+};
+
 
 let titel_2 = prompt("Titel: ");	
 let typ_2 = prompt("Typ (Einahme oder Ausgabe):");
@@ -17,13 +33,30 @@ let date_2 = prompt("Datum (jjjj-mm-tt): ");
 console.log(`Titel: ${titel_2}
     Typ: ${typ_2}
     Betrag: ${betrag_2}
-    Date: ${date_2}`);	
+    Date: ${date_2}`
+);
+
+if (typ_2 === "Einnahme") {
+    income = income + betrag_2;    	
+} else if (typ_2 === "Ausgabe") {
+    expense = expense + betrag_2;
+    balance = balance - betrag_2;
+
+} else {
+    console.log(`Der Typ_"${typ_2}" ist nicht bekannt`);
+};
+    
 
 //Gesamtbilanz
-let income;
-let expense;
-let balance = betrag_1 + betrag_2;
-console.log(`Gesamtbilanz: ${balance}`);
-
 let positiv = balance >= 0;
-console.log(`Bilanz ist positiv: ${positiv}`);
+
+console.log(`
+    Einnahme: ${income} ct
+    Ausgabe: ${expense} ct
+    Gesamtbilanz: ${balance} ct
+    Bilanz ist positiv: ${positiv}`
+);
+
+
+
+
